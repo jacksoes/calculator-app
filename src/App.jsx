33878d59538df1a2fb52c80f8541fd4c7e5d3a89
jack soesman <jsoesman1@yahoo.com>
2024-05-10 16:06:40 -0400

    create layout for output screen and buttons

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+import { Button, Container } from 'react-bootstrap'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='main-flex'>
+        <div className="container-rectangle-vert">
+            <div Id="calculator-output">
+              
+              </div> 
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
